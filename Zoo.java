39a80498608e42561e54c0377528742449b7f17d
yasminederouiche <165273786+yasminederouiche@users.noreply.github.com>
2025-09-20 17:08:00 +0200

    Add files via upload

---
+++ Zoo.java
@@ -0,0 +1,28 @@
+public class Zoo {
+
+    Animal[] animals;
+    String name;
+    String city;
+    int nbrCages;
+    public Zoo(){}
+    public Zoo(String name, String city, int nbrCages) {
+        this.name = name;
+        this.city = city;
+        this.nbrCages = nbrCages;
+        this.animals = new Animal[25];
+    }
+
+    public void displayZoo() {
+        System.out.println("Nom du zoo : " + name);
+        System.out.println("Ville : " + city);
+        System.out.println("Nombre de cages : " + nbrCages);
+    }
+
+    public String toString() {
+        return "Zoo{" +
+                "name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", nbrCages=" + nbrCages +
+                '}';
+    }
+}
